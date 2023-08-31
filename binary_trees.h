497e155577d8ccd2dd4420843f24b71feb7c457d
mooka007 <mooka3084@gmail.com>
2023-08-31 10:04:12 -0700

    0 binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -1,27 +1,15 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
-
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 #include <stdlib.h>
 #include <stddef.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <math.h>
-#include <limits.h>
-
-
-
 /**
  * struct binary_tree_s - Binary tree node
- * Author - MoOka
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-
-
 struct binary_tree_s
 {
 	int n;
@@ -29,13 +17,15 @@ struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 };
+/*Basic Binary Tree*/
 typedef struct binary_tree_s binary_tree_t;
+/*Binary Search Tree*/
 typedef struct binary_tree_s bst_t;
+/*AVL Tree*/
 typedef struct binary_tree_s avl_t;
+/*Max Binary Heap*/
 typedef struct binary_tree_s heap_t;
-
 void binary_tree_print(const binary_tree_t *);
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -55,3 +45,4 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+#endif
